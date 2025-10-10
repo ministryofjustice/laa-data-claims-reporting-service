13a11182567bf218c449eddcad8a53e951ae7423
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-10-10 11:16:19 +0100

    Grant required privileges to reporting_user

--- laa-data-claims-reporting-service/src/main/resources/db/migration/V3__start_replication.sql
+++ laa-data-claims-reporting-service/src/main/resources/db/migration/V3__start_replication.sql
@@ -1,3 +1,9 @@
+GRANT USAGE ON SCHEMA claims TO reporting_user;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA claims TO reporting_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA claims GRANT SELECT ON TABLES TO reporting_user;
+
 CREATE SUBSCRIPTION claims_reporting_service_sub
 CONNECTION 'host=${replication_source_db_url} port=5432 dbname=${replication_source_db_name} user=${reporting_username} password=${reporting_password}'
 PUBLICATION claims_reporting_service_pub
