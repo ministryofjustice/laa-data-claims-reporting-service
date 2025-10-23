f7a6ca10dd384befae172ca2ffe274e73e637109
sinead.cummings <sinead.cummings@justice.gov.uk>
2025-10-23 13:01:47 +0100

    removed unused import

--- src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/CsvRowCallbackHandlerTest.java
+++ src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/CsvRowCallbackHandlerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.dstew.claimsreports.service;
 
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
