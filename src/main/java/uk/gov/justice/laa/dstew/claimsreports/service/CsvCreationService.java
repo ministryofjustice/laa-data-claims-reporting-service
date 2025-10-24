0de617fdc28b67b8d8c062085d65eb3d5b1f520f
SineadCummings <sinead.cummings@justice.gov.uk>
2025-10-24 11:34:38 +0100

    DSTEW-479: Part 3 - Create csv service and integrate with resport service (#26)
    * moved files to top dir to allow running without errors when csv creation code added
    
    * Add row handler and tests, exceptions and app config required for these changes and for csv creation service
    
    * Add service to create csv file from db
    
    * rename app config values for increased clarity
    
    * add documentation to row handler
    
    * add documentation to row handler test
    
    * correct and improve documentation
    
    * Remove redundant code post review
    
    * Update to remove redundant check now default is built into config
    
    * removed unused import
    
    * Combine csv creation code with report service code
    
    * use valid file directory
    
    * improve documentation
    
    * Updates post code review

---
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/service/CsvCreationService.java
@@ -0,0 +1,93 @@
+package uk.gov.justice.laa.dstew.claimsreports.service;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.dstew.claimsreports.config.AppConfig;
+import uk.gov.justice.laa.dstew.claimsreports.exception.CsvCreationException;
+
+/**
+ * Data access object class to provide interface between application and database layer.
+ */
+@Service
+@Slf4j
+@AllArgsConstructor
+public class CsvCreationService {
+  private final JdbcTemplate jdbcTemplate;
+  private final DataSource dataSource;
+  protected AppConfig appConfig;
+
+  /**
+   * Builds CSV from data retrieved from SQL query
+   * Returns data in chunks, size defined in application config, to ensure
+   * good performance for large datasets.
+   *
+   * @param sqlQuery query for retrieving dataset
+   * @param writer writes string buffer into csv file
+   */
+  public void buildCsvFromData(String sqlQuery, BufferedWriter writer) {
+    if (sqlQuery == null || sqlQuery.trim().isEmpty()) {
+      throw new CsvCreationException("SQL query is not provided");
+    }
+
+    if (writer == null) {
+      throw new CsvCreationException("BufferedWriter is null");
+    }
+
+    try (writer) {
+      StringBuilder line = new StringBuilder();
+
+      jdbcTemplate.query(
+          (Connection con) -> {
+            return buildPreparedStatement(sqlQuery, con, appConfig.getDataChunkSize());
+          },
+          new CsvRowCallbackHandler(writer, line, appConfig.getBufferFlushFrequency())
+      );
+
+      writer.flush();
+
+      log.info("CSV creation completed");
+
+    } catch (IOException ex) {
+      throw new CsvCreationException("Failure to write to file: " + ex.getMessage());
+    } catch (CsvCreationException ex) {
+      throw ex;
+    } catch (Exception ex) {
+      throw new CsvCreationException("Failure during CSV creation " + ex.getMessage());
+    }
+  }
+
+  /**
+   * Creates a prepared statement that fetches data from the database in defined chunks,
+   * specifically to provide performance improvements for large data sets.
+   * Setting autocommit to false, in combination with fetch size,
+   * ensures data is retrieved in chunks.
+   *
+   * @param sqlQuery SELECT statement for report data
+   * @param con database connection
+   *
+   * @return {PreparedStatement}
+   */
+  private PreparedStatement buildPreparedStatement(
+      String sqlQuery, Connection con, int dataChunkSize) {
+    try {
+      con.setAutoCommit(false);
+      PreparedStatement statement = con.prepareStatement(sqlQuery,
+          ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+      statement.setFetchSize(dataChunkSize);
+      return statement;
+    } catch (SQLException ex) {
+      throw new CsvCreationException("Failed on creation of prepared statement "
+          + ex.getNextException());
+    }
+  }
+
+}
