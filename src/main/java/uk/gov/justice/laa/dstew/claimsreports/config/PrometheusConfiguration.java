ff7e1869826749255913492c91db97105f3e1c13
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-01-14 17:16:02 +0000

    LPF-1227 - resolve infos clogging up log

--- src/main/java/uk/gov/justice/laa/dstew/claimsreports/config/PrometheusConfiguration.java
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/config/PrometheusConfiguration.java
@@ -1,12 +1,16 @@
 package uk.gov.justice.laa.dstew.claimsreports.config;
 
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusRenameFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Prometheus Registry allowing for integration with pushgateway.
  */
+@Slf4j
 @Configuration
 public class PrometheusConfiguration {
 
@@ -14,4 +18,14 @@ public class PrometheusConfiguration {
   public PrometheusMeterRegistry prometheusMeterRegistry() {
     return new PrometheusMeterRegistry(key -> null);
   }
+
+  /*
+    This is due to micrometer, but without fixing it we get multiple infos a second,
+    which is obviously unideal and masks any actual issues.
+   */
+  @Bean
+  MeterFilter fixInfoLogsAboutStartTimeMessage() {
+    return new PrometheusRenameFilter();
+  }
+
 }
