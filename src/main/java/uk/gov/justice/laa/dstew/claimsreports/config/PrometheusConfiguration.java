1baf576d6d1a7be2c63d140771f535b89c7ffce0
Chris Cook <114592674+ChrisCookOC@users.noreply.github.com>
2026-01-15 11:00:30 +0000

    LPF-1227 - resolve Prometheus infos clogging up log (#72)
    * LPF-1227 - resolve infos clogging up log
    
    * LPF-1227 - remove unused logging

--- src/main/java/uk/gov/justice/laa/dstew/claimsreports/config/PrometheusConfiguration.java
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/config/PrometheusConfiguration.java
@@ -1,6 +1,8 @@
 package uk.gov.justice.laa.dstew.claimsreports.config;
 
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusRenameFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,4 +16,13 @@ public class PrometheusConfiguration {
   public PrometheusMeterRegistry prometheusMeterRegistry() {
     return new PrometheusMeterRegistry(key -> null);
   }
+
+  /*
+    This updates the help message associated with micrometers messages, so that we don't get 3 infos a second clogging up the logs
+   */
+  @Bean
+  MeterFilter fixInfoLogsAboutStartTimeMessage() {
+    return new PrometheusRenameFilter();
+  }
+
 }
