0401afa57c376131d4106d3a9896a5cc13ecc9fc
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-01-19 14:46:32 +0000

    LPF-1246 - report 13 v2 service

---
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/service/Report013v2Service.java
@@ -0,0 +1,66 @@
+package uk.gov.justice.laa.dstew.claimsreports.service;
+
+import java.time.Clock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
+import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
+
+/**
+ * Report013v2Service is responsible for generating and managing version 2 of report_013.
+ * This service extends the AbstractReportService and provides
+ * an implementation for the report generation process.
+ * Responsibilities:
+ * - Implements report generation logic for Report013 data.
+ * - Utilizes the inherited functionality to refresh materialized views as needed.
+ */
+@Slf4j
+@Service
+public class Report013v2Service extends AbstractReportService {
+
+  public Report013v2Service(JdbcTemplate jdbcTemplate,
+                            S3ClientWrapper s3ClientWrapper, CsvCreationService csvCreationService, MetricsHandler metricsHandler, Clock clock) {
+    super(jdbcTemplate, s3ClientWrapper, csvCreationService, metricsHandler, clock);
+  }
+
+  @Override
+  protected String getDataSourceName() {
+    return "claims.report_013";
+  }
+
+  /**
+   * This is a complex report with variable columns and needs a stored function.
+   * The function refreshes the underlying report data in a table, rather than a materialised view.
+   */
+  @Override
+  protected String getRefreshCommand() {
+    return "SELECT claims.refresh_report013()"; //The "SELECT" statement actually runs the stored function
+  }
+
+  @Override
+  protected String getReportFileName() {
+    return "report_013_v2";
+  }
+
+  @Override
+  protected String getReportName() {
+    return "REPORT013V2";
+  }
+
+  @Override
+  protected String getReportFolder() {
+    return "daily";
+  }
+
+  @Override
+  protected String getOrderByClause() {
+    return " \"Provider Office Account Number\", \"Area of Law\"";
+  }
+
+  // Daily report
+  @Override
+  protected boolean runToday() {
+    return true;
+  }
+}
---
+++ src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report013v2ServiceTest.java
@@ -0,0 +1,70 @@
+package uk.gov.justice.laa.dstew.claimsreports.service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
+import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * Unit tests for {@link Report013Service}.
+ */
+class Report013v2ServiceTest {
+
+  private Report013v2Service service;
+  private JdbcTemplate jdbcTemplate;
+  private CsvCreationService creationService;
+  private S3ClientWrapper s3ClientWrapper;
+  private MetricsHandler metricsHandler;
+  private Clock fixedClock;
+
+  @BeforeEach
+  void setUp() {
+    jdbcTemplate = mock(JdbcTemplate.class);
+    creationService = mock(CsvCreationService.class);
+    s3ClientWrapper = mock(S3ClientWrapper.class);
+    metricsHandler = mock(MetricsHandler.class);
+
+    Instant fixedNow = Instant.parse("2025-12-22T10:00:00Z");
+    fixedClock = Clock.fixed(fixedNow, ZoneOffset.UTC);
+
+    service = new Report013v2Service(jdbcTemplate, s3ClientWrapper, creationService, metricsHandler, fixedClock);
+  }
+
+  @Test
+  void refreshDataSource_ShouldCallRepositoryMethod() {
+    // when
+    service.refreshDataSource();
+    // then
+    verify(jdbcTemplate, times(1))
+        .execute("SELECT claims.refresh_report013()");
+    verifyNoMoreInteractions(jdbcTemplate);
+  }
+
+  @Test
+  void generateReport_shouldCallTheRightServicesWithTheRightValues(){
+
+    service.generateReport();
+
+    verify(creationService).buildCsvFromData(
+        eq("SELECT * FROM claims.report_013"
+            + " ORDER BY  \"Provider Office Account Number\", \"Area of Law\""),
+        any(BufferedWriter.class),
+        any()
+    );
+    verify(s3ClientWrapper).uploadFile(any(File.class), eq("reports/daily/report_013_v2_2025-12-22.csv"));
+  }
+
+}
