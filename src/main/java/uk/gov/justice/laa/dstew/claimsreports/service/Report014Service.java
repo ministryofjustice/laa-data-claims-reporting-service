5febb0e52763da6bd6974c23f68139955a455c37
Chris Cook <christopher.cook@digital.justice.gov.uk>
2026-02-16 11:12:44 +0000

    LPF-1253 - rep014

---
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/service/Report014Service.java
@@ -0,0 +1,64 @@
+package uk.gov.justice.laa.dstew.claimsreports.service;
+
+import java.time.Clock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
+import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
+
+/**
+ * Report014Service is responsible for generating and managing report_014.
+ * This service extends the AbstractReportService and provides
+ * an implementation for the report generation process.
+ * Responsibilities:
+ * - Implements report generation logic for Report014 data.
+ * - Utilizes the inherited functionality to refresh materialized views as needed.
+ */
+@Slf4j
+@Service
+public class Report014Service extends AbstractReportService {
+
+  public Report014Service(JdbcTemplate jdbcTemplate,
+                          S3ClientWrapper s3ClientWrapper, CsvCreationService csvCreationService, MetricsHandler metricsHandler, Clock clock) {
+    super(jdbcTemplate, s3ClientWrapper, csvCreationService, metricsHandler, clock);
+  }
+
+  @Override
+  protected String getDataSourceName() {
+    return "claims.mvw_report_014";
+  }
+
+  @Override
+  protected String getRefreshCommand() {
+    return "REFRESH MATERIALIZED VIEW claims.mvw_report_014";
+  }
+
+  @Override
+  protected String getReportFileName() {
+    return "report_014";
+  }
+
+  @Override
+  public String getReportName() {
+    return "REPORT014";
+  }
+
+  @Override
+  protected String getReportFolder() {
+    return "daily";
+  }
+
+  @Override
+  protected String getOrderByClause() {
+    return " \"Claim ID\","
+        + "    to_char(to_date(\"Amendment Date\", 'DD/MM/YYYY HH24:MI:SS'), 'YYYYMM'),"
+        + "    \"Assessment ID\"";
+  }
+
+  // Daily report
+  @Override
+  protected boolean runToday() {
+    return true;
+  }
+}
---
+++ src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report014ServiceTest.java
@@ -0,0 +1,70 @@
+package uk.gov.justice.laa.dstew.claimsreports.service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
+import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * Unit tests for {@link Report014Service}.
+ */
+class Report014ServiceTest {
+
+  private Report014Service service;
+  private JdbcTemplate jdbcTemplate;
+  private CsvCreationService creationService;
+  private S3ClientWrapper s3ClientWrapper;
+  private MetricsHandler metricsHandler;
+  private Clock fixedClock;
+
+  @BeforeEach
+  void setUp() {
+    jdbcTemplate = mock(JdbcTemplate.class);
+    creationService = mock(CsvCreationService.class);
+    s3ClientWrapper = mock(S3ClientWrapper.class);
+    metricsHandler = mock(MetricsHandler.class);
+
+    Instant fixedNow = Instant.parse("2025-12-22T10:00:00Z");
+    fixedClock = Clock.fixed(fixedNow, ZoneOffset.UTC);
+
+    service = new Report014Service(jdbcTemplate, s3ClientWrapper, creationService, metricsHandler, fixedClock);
+  }
+
+  @Test
+  void refreshDataSource_ShouldCallRepositoryMethod() {
+    // when
+    service.refreshDataSource();
+    // then
+    verify(jdbcTemplate, times(1))
+        .execute("REFRESH MATERIALIZED VIEW claims.mvw_report_014");
+    verifyNoMoreInteractions(jdbcTemplate);
+  }
+
+  @Test
+  void generateReport_shouldCallTheRightServicesWithTheRightValues(){
+
+    service.generateReport();
+
+    verify(creationService).buildCsvFromData(
+        eq("SELECT * FROM claims.mvw_report_014"
+            + " ORDER BY  \"Claim ID\",    to_char(to_date(\"Amendment Date\", 'DD/MM/YYYY HH24:MI:SS'), 'YYYYMM'),    \"Assessment ID\""),
+        any(BufferedWriter.class),
+        any()
+    );
+    verify(s3ClientWrapper).uploadFile(any(File.class), eq("reports/daily/report_014_2025-12-22.csv"));
+  }
+
+}
