d7f7840e4fd82b8e58002c3f3032e304f5388ac5
Masuk Kazi <90158744+masuk-kazi98@users.noreply.github.com>
2026-02-23 18:21:09 +0000

    fix tests v5

--- src/integrationTest/java/uk/gov/justice/laa/dstew/claimsreports/sql/Report013IntegrationTest.java
+++ src/integrationTest/java/uk/gov/justice/laa/dstew/claimsreports/sql/Report013IntegrationTest.java
@@ -62,10 +62,10 @@ class Report013IntegrationTest extends IntegrationTestBase {
             ));
 
     assertThat(countsByAreaOfLaw)
-        .containsKeys("CIVIL", "CRIME LOWER");
+        .containsKeys("LEGAL_HELP", "CRIME_LOWER");
 
-    assertThat(countsByAreaOfLaw.get("CIVIL")).isEqualTo(1L);
-    assertThat(countsByAreaOfLaw.get("CRIME LOWER")).isEqualTo(1L);
+    assertThat(countsByAreaOfLaw.get("LEGAL_HELP")).isEqualTo(1L);
+    assertThat(countsByAreaOfLaw.get("CRIME_LOWER")).isEqualTo(1L);
 
     long totalRows =
         countsByAreaOfLaw.values().stream().mapToLong(Long::longValue).sum();
@@ -106,7 +106,7 @@ class Report013IntegrationTest extends IntegrationTestBase {
 
     Map<String, Object> civilRow =
         report013Rows.stream()
-            .filter(r -> "CIVIL".equals(r.get("Area of Law")))
+            .filter(r -> "LEGAL_HELP".equals(r.get("Area of Law")))
             .findFirst()
             .orElseThrow();
 
@@ -121,7 +121,7 @@ class Report013IntegrationTest extends IntegrationTestBase {
 
     Map<String, Object> crimeRow =
         report013Rows.stream()
-            .filter(r -> "CRIME LOWER".equals(r.get("Area of Law")))
+            .filter(r -> "CRIME_LOWER".equals(r.get("Area of Law")))
             .findFirst()
             .orElseThrow();
 
@@ -167,7 +167,7 @@ class Report013IntegrationTest extends IntegrationTestBase {
 
     Map<String, Object> crimeRow =
         report013Rows.stream()
-            .filter(r -> "CRIME LOWER".equals(r.get("Area of Law")))
+            .filter(r -> "CRIME_LOWER".equals(r.get("Area of Law")))
             .findFirst()
             .orElseThrow();
 
@@ -192,7 +192,7 @@ class Report013IntegrationTest extends IntegrationTestBase {
         '11111111-1111-1111-1111-111111111111',
         'OA001',
         'MAR-2025',
-        'LEGAL HELP',
+        'CIVIL',
         'VALIDATION_SUCCEEDED',
         'CSN001',
         NULL,
