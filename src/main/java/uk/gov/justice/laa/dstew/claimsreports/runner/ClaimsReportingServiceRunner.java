6adda043422eb3d3baf315f01a5ca24adbd7e733
Masuk Kazi <90158744+masuk-kazi98@users.noreply.github.com>
2026-06-17 09:55:21 +0100

    chore: checkstyle

--- src/main/java/uk/gov/justice/laa/dstew/claimsreports/runner/ClaimsReportingServiceRunner.java
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/runner/ClaimsReportingServiceRunner.java
@@ -6,7 +6,6 @@ import static uk.gov.justice.laa.dstew.claimsreports.config.PrometheusConfigurat
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
