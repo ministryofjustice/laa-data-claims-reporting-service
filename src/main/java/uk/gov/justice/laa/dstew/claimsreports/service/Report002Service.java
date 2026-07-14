2dd8e1217d29af58203f13d9d48795fd30ae499e
Masuk Kazi <90158744+masuk-kazi98@users.noreply.github.com>
2026-07-14 10:06:48 +0100

    feat(LPF-1530): add force run feature flag

--- src/main/java/uk/gov/justice/laa/dstew/claimsreports/service/Report002Service.java
+++ src/main/java/uk/gov/justice/laa/dstew/claimsreports/service/Report002Service.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.dstew.claimsreports.config.AppConfig;
 import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
 import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
 
@@ -16,13 +17,16 @@ import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
 public class Report002Service extends AbstractReportService {
 
   private static final int MONTHLY_REPORT_DATE = 21;
+  private final AppConfig appConfig;
 
   public Report002Service(JdbcTemplate jdbcTemplate,
                           S3ClientWrapper s3ClientWrapper,
                           CsvCreationService csvCreationService,
                           MetricsHandler metricsHandler,
-                          Clock clock) {
+                          Clock clock,
+                          AppConfig appConfig) {
     super(jdbcTemplate, s3ClientWrapper, csvCreationService, metricsHandler, clock);
+    this.appConfig = appConfig;
   }
 
   @Override
@@ -61,6 +65,11 @@ public class Report002Service extends AbstractReportService {
 
   @Override
   protected boolean runToday() {
-    return LocalDate.now(clock).getDayOfMonth() == MONTHLY_REPORT_DATE;
+    if (appConfig.isForceRunReport002()) {
+      log.info("Force run for Report002 is enabled. Running report regardless of date.");
+    } else {
+      log.info("Force run for Report002 is disabled. Running report only if today is the {}th of the month.", MONTHLY_REPORT_DATE);
+    }
+    return appConfig.isForceRunReport002() || LocalDate.now(clock).getDayOfMonth() == MONTHLY_REPORT_DATE;
   }
 }
--- src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report002ServiceTest.java
+++ src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report002ServiceTest.java
@@ -17,6 +17,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import uk.gov.justice.laa.dstew.claimsreports.config.AppConfig;
 import uk.gov.justice.laa.dstew.claimsreports.config.MetricsHandler;
 import uk.gov.justice.laa.dstew.claimsreports.service.s3.S3ClientWrapper;
 
@@ -31,6 +32,7 @@ class Report002ServiceTest {
   private S3ClientWrapper s3ClientWrapper;
   private MetricsHandler metricsHandler;
   private Clock fixedClock;
+  private AppConfig appConfig;
 
   @BeforeEach
   void setUp() {
@@ -38,11 +40,14 @@ class Report002ServiceTest {
     creationService = mock(CsvCreationService.class);
     s3ClientWrapper = mock(S3ClientWrapper.class);
     metricsHandler = mock(MetricsHandler.class);
+    appConfig = mock(AppConfig.class);
 
     Instant fixedNow = Instant.parse("2025-12-21T10:00:00Z");
     fixedClock = Clock.fixed(fixedNow, ZoneOffset.UTC);
 
-    service = new Report002Service(jdbcTemplate, s3ClientWrapper, creationService, metricsHandler, fixedClock);
+    org.mockito.Mockito.when(appConfig.isForceRunReport002()).thenReturn(false);
+
+    service = new Report002Service(jdbcTemplate, s3ClientWrapper, creationService, metricsHandler, fixedClock, appConfig);
   }
 
   @Test
@@ -85,9 +90,29 @@ class Report002ServiceTest {
         s3ClientWrapper,
         creationService,
         metricsHandler,
-        fixedClock
+        fixedClock,
+        appConfig
     );
 
     Assertions.assertFalse(serviceOn22nd.runToday());
+  }
+
+  @Test
+  void returnsTrueOnThe22ndOfMonthIfOverrideSet() {
+    org.mockito.Mockito.when(appConfig.isForceRunReport002()).thenReturn(true);
+
+    Instant fixedNow = Instant.parse("2025-12-22T10:00:00Z");
+    fixedClock = Clock.fixed(fixedNow, ZoneOffset.UTC);
+
+    Report002Service serviceOn22nd = new Report002Service(
+        jdbcTemplate,
+        s3ClientWrapper,
+        creationService,
+        metricsHandler,
+        fixedClock,
+        appConfig
+    );
+
+    Assertions.assertTrue(serviceOn22nd.runToday());
   }
 }
