ec60b3d54681c2fbf56c7f192fed483ce06cb643
AlastairMOJ <Alastair.Mottram-Epson@justice.gov.uk>
2026-09-18 17:07:49 +0100

    feat(LPF-1555): the changes have been made, the code use the correct dependency for the service test now.

--- src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report014ServiceTest.java
+++ src/test/java/uk/gov/justice/laa/dstew/claimsreports/service/Report014ServiceTest.java
@@ -1,8 +1,8 @@
 package uk.gov.justice.laa.dstew.claimsreports.service;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
